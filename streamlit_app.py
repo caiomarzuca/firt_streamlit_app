6bd2a609989fdc39d7efbc987e4e620387fd03be
caiomarzuca <63164319+caiomarzuca@users.noreply.github.com>
2022-08-01 11:59:31 -0300

    Update streamlit_app.py
    change de list index to 'Fruit' column

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Read and display dataframe 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # List of fruits to choose
 streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index))
