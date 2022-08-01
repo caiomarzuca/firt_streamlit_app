3ed4221e4a623f94b7e59bcaef46f466f81d1220
caiomarzuca <63164319+caiomarzuca@users.noreply.github.com>
2022-08-01 11:46:45 -0300

    Update streamlit_app.py
    added a list to choose a fruit by index

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,9 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocato Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# List of fruits to choose
+streamlit.multselect("Pick up some fruit:", list(my_fruit_list.index))
+
+#Read and display dataframe 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
